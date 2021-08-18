6dd464399a9cbe090ea2b24cbf2edaf0cf5bbff5
Sebastian Fredriksson Bernholtz <sebefre@gmail.com>
2021-08-18 12:59:05 +1000

    add test-confg retries and base-url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4
+testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --test-config $5 --retries $6 --base-url $7
